62756be1946f7101b7a6a20006327659991b7a7d
JReynaldoDelRio <reynaldodelriocontreras@gmail.com>
2024-08-01 23:04:16 -0500

    iniciando2

---
+++ src/main/java/com/delrio/chatiTienda2/ChatiTienda2Application.java
@@ -0,0 +1,13 @@
+package com.delrio.chatiTienda2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatiTienda2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatiTienda2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/delrio/chatiTienda2/ChatiTienda2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delrio.chatiTienda2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatiTienda2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
